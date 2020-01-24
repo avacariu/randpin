007119ae639ce71b66123d7324d7e219df3d279e
Andrei Vacariu <andrei@vacariu.ca>
2020-01-23 23:15:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Random Pinboard
+===============
+
+This project is simply a tool for opening a random unread Pinboard bookmark
+using your default browser.
+
+
+License
+-------
+
+This project is licensed under the MIT license. See the LICENCE file for
+details.
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open("src/randpin/version.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    name='randpin',
+    version=version['__version__'],
+    author='Andrei Vacariu',
+    author_email='andrei@vacariu.ca',
+    description='A simple tool for opening a random unread bookmark from Pinboard',
+
+    python_requires='>=3.5',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+
+    entry_points={
+        'console_scripts': [
+            'randpin=randpin.__main__:main',
+        ],
+    },
+
+    install_requires=[
+        'click',
+        'pinboard',
+    ],
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
